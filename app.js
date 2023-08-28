902d339f16bcb20c393a88e8d93261635033adb6
Elad Hamneshin <hamneshin123@gmail.com>
2023-08-28 16:21:13 +0300

    {update}-remove imports

--- app.js
+++ app.js
@@ -10,28 +10,22 @@ import cors from "cors";
 const app = express();
 const port = 3000;
 
-// function initServer(app, port) {
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(morgan('dev'))
-    app.use("/products", productsRouter);
-    app.use("/users", userRouter);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'))
+app.use("/products", productsRouter);
+app.use("/users", userRouter);
+app.listen(port, () => {
+    axios.get("https://fakestoreapi.com/products")
+    .then((res) => {
+        const products = res.data;
+        for(const product of products)
+            product.quantity = Math.floor(Math.random() * 100);
+    
+        return productsDL.addProducts(products).then(() => console.log("Products DB is ready"));
+    })
+    .then(()=>console.log(`Server is up and running on port:${port}`));
 
-    app.listen(port, () => {
-        axios.get("https://fakestoreapi.com/products")
-        .then((res) => {
-            const products = res.data;
-            for(const product of products)
-                product.quantity = Math.floor(Math.random() * 100);
-        
-            return productsDL.addProducts(products).then(() => console.log("Products DB is ready"));
-        })
-        .then(()=>console.log(`Server is up and running on port:${port}`));
-
-        usersDL.retriveDb().then(() => console.log("Users DB is ready"));
-    });
-
-// }
-
-// initServer(app, port);
+    usersDL.retriveDb().then(() => console.log("Users DB is ready"));
+});
